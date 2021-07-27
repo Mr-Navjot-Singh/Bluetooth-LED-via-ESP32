efd0b1740bc927b62ea0da91e188d64b0a748903
Mr-Navjot-Singh <66953565+Mr-Navjot-Singh@users.noreply.github.com>
2021-07-27 22:15:57 +0530

    Add files via upload

---
+++ ESP32_BL_LED_Control.ino
@@ -0,0 +1,45 @@
+
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+// Handle received and sent messages
+String message = "";
+char incomingChar;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  
+  // Read received messages (LED control command)
+  if (SerialBT.available()){
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n'){
+      message += String(incomingChar);
+    }
+    else{
+      message = "";
+    }
+    Serial.write(incomingChar);  
+  }
+  // Check received message and control output accordingly
+  if (message =="led_on"){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else if (message =="led_off"){
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  delay(20);
+}
